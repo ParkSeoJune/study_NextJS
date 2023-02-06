0a6fb6d833504503fe211431cfae2fc46a3de869
ParkSeoJune <tjwns4122@gmail.com>
2023-02-06 18:06:22 +0900

    Link 컴포넌트 사용해보기

--- hello-world/pages/index.tsx
+++ hello-world/pages/index.tsx
@@ -1,12 +1,17 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <div>
+        <h1>Home Page</h1>
+        <Link href={"/blog"}>
+          <div>Blog</div>
+        </Link>
+        <Link href={"/product"}>
+          <div>Products</div>
+        </Link>
+      </div>
     </>
   );
 }
--- hello-world/pages/product/index.tsx
+++ hello-world/pages/product/index.tsx
@@ -1,9 +1,31 @@
-function ProductList() {
+import Link from "next/link";
+
+function ProductList({ productId = 100 }) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href={"/"}>
+        <div>Home</div>
+      </Link>
+      <h2>
+        <Link href={"/product/1"}>
+          <div>Product 1</div>
+        </Link>
+      </h2>
+      <h2>
+        <Link href={"/product/2"}>
+          <div>Product 2</div>
+        </Link>
+      </h2>
+      <h2>
+        <Link href={"/product/3"} replace>
+          <div>Product 3</div>
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/product/${productId}`}>
+          <div>Product {productId}</div>
+        </Link>
+      </h2>
     </>
   );
 }
