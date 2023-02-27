4a1195a22cdc2a00e8f76728d9f867a722b59290
ParkSeoJune <tjwns4122@gmail.com>
2023-02-27 16:28:28 +0900

    페이지를 정적 생성했을 때 데이터가 업데이트 되어도 페이지는 변경되지 않는다

---
+++ next-pre-rendering/pages/products/[productId].tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+
+function Product({ product }: any) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <h2>
+        {product.id} {product.title} {product.price}
+      </h2>
+      <p>{product.description}</p>
+    </>
+  );
+}
+
+export default Product;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { productId: "1" },
+      },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context: any) {
+  const { params } = context;
+  const response = await fetch(
+    `http://localhost:4000/products/${params.productId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      product: data,
+    },
+  };
+}
---
+++ next-pre-rendering/pages/products/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+function ProductList({ products }: any) {
+  return (
+    <>
+      <h1>List of Products</h1>
+      {products.map((product: any) => {
+        return (
+          <div key={product.id}>
+            <h2>
+              {product.id} {product.title} {product.price}
+            </h2>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default ProductList;
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:4000/products");
+  const data = await response.json();
+
+  return {
+    props: {
+      products: data,
+    },
+  };
+}
