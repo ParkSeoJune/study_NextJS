651b424b9dcd6a51af2397f1527d4b6be1d93616
ParkSeoJune <tjwns4122@gmail.com>
2023-08-02 18:14:53 +0900

    #1.3 Routing

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "@/components/NavBar";
+
 export default function Potato() {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
