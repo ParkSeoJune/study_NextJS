27a1d236001f75601bd72687553cdac8a6d92bf0
ParkSeoJune <tjwns4122@gmail.com>
2023-08-07 18:04:35 +0900

    #2.0 && #2.1 Patterns, Fetching data

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,9 +1,31 @@
-import NavBar from "@/components/NavBar";
+import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "0bdbe0cbefac407bbf279646b8de93ea";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Seo title="Home" />
+      {!movies.length && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
