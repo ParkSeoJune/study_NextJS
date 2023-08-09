1036ced0a67a79503a43087a37c22ff7326655d7
ParkSeoJune <tjwns4122@gmail.com>
2023-08-09 16:09:09 +0900

    redirect

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path",
+        destination: "/new-blog/:path",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
