4f57eba9fc073f441ae5fd97b7a3ac73cad0f477
ParkSeoJune <tjwns4122@gmail.com>
2023-08-10 16:01:07 +0900

    #2.3 Server Side Rendering

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -3,21 +3,14 @@ import { useEffect, useState } from "react";
 
 const API_KEY = "0bdbe0cbefac407bbf279646b8de93ea";
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
 
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies.length && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -46,4 +39,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
